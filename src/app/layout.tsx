f39e5f44710f9f39f8fe4d75283098ddc45f0bfe
batu-el <bel@tulane.edu>
2025-09-03 04:07:09 -0700

    dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="min-h-screen flex flex-col">
           <div className="flex-1">
